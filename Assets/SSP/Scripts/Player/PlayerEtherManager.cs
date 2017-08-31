4f51d6ec64d5e95a892e2f9ae96ee821accf611c
chromee <odk.mmmm@gmail.com>
2017-09-01 01:22:21 +0900

    PlayerEtherManager作成・EtherModel削除

---
+++ Assets/SSP/Scripts/Player/PlayerEtherManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEtherManager : MonoBehaviour, IEther
+{
+    [SerializeField] private float initialEther;
+    private PlayerModel palyerModel;
+
+    private void Start()
+    {
+        palyerModel = GetComponent<PlayerModel>();
+        palyerModel.Ether.Value = initialEther;
+    }
+
+    public void SetEther(float ether)
+    {
+        palyerModel.Ether.Value = ether;
+    }
+
+    public float GetEther()
+    {
+        return palyerModel.Ether.Value;
+    }
+}
