8a1cc2e3aaa3b03d81b9dd79c1a5c8383177ddad
chromee <odk.mmmm@gmail.com>
2017-09-01 01:25:55 +0900

    Playerプレハブ修正

--- Assets/SSP/Scripts/Player/PlayerModel.cs
+++ Assets/SSP/Scripts/Player/PlayerModel.cs
@@ -9,4 +9,13 @@ public class PlayerModel : MonoBehaviour
     public ReactiveProperty<float> Health = new ReactiveProperty<float>();
     public ReactiveProperty<float> Ether = new ReactiveProperty<float>();
 
+    //デバッグ用（インスペクタで値をみるため）
+    [SerializeField] private float currentHealth;
+    [SerializeField] private float currentEther;
+
+    private void Start()
+    {
+        Health.Subscribe(v => currentHealth = v);
+        Ether.Subscribe(v => currentEther = v);
+    }
 }
