a8dbd730833e804578ae9939875807569c09dea1
chromee <odk.mmmm@gmail.com>
2017-09-01 01:00:22 +0900

    PlayerModel作成

---
+++ Assets/SSP/Scripts/Player/PlayerModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class PlayerModel : MonoBehaviour
+{
+
+    public ReactiveProperty<float> Health = new ReactiveProperty<float>();
+    public ReactiveProperty<float> Ether = new ReactiveProperty<float>();
+
+}
