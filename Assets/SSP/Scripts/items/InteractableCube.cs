315b4532481f44340a3d9e727fb8729c9b269db4
RauziDssh <bmn1070@mail4.doshisha.ac.jp>
2017-09-02 16:34:49 +0900

    Interaction機能の実装

---
+++ Assets/SSP/Scripts/items/InteractableCube.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableCube : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        this.gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0.0f,10.0f,0.0f));
+    }
+}
