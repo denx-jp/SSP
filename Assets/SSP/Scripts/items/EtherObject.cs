858d02edbe74182c11a1565da7ed19e5675f1ac2
chromee <odk.mmmm@gmail.com>
2017-09-02 16:58:34 +0900

    エーテルのサイズからトリガーのサイズを変更するように

--- Assets/SSP/Scripts/items/EtherObject.cs
+++ Assets/SSP/Scripts/items/EtherObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
+using System.Linq;
 
 public class EtherObject : MonoBehaviour
 {
@@ -10,29 +11,36 @@ public class EtherObject : MonoBehaviour
     public float etherValue;
 
     [SerializeField] private float originEtherSize;
+    [SerializeField] private float triggerSize = 0.5f;
     [SerializeField] private float floatHeight;
-    private Rigidbody rigid;
+    private SphereCollider trigger;
+
     private RaycastHit hit;
     private int layerMask = 1 << LayerMap.Stage;
 
     private void Start()
     {
-        rigid = GetComponent<Rigidbody>();
-        var col = GetComponent<SphereCollider>();
+        var rigid = GetComponent<Rigidbody>();
 
         this.UpdateAsObservable()
             .Where(_ => Physics.Raycast(transform.position, Vector3.down, out hit, floatHeight * 10, layerMask))
             .Where(_ => Vector3.Distance(transform.position, hit.point) < floatHeight)
             .Take(1)
-            .Subscribe(_ =>
-            {
-                rigid.useGravity = false;
-            });
+            .Subscribe(_ => rigid.useGravity = false);
     }
 
-    public void SetEther(float value)
+    public void Init(float value)
     {
         etherValue = value;
         transform.localScale = Vector3.one * originEtherSize * value;
+        if (transform.localScale.x < 1)
+            GetTrigger().radius = triggerSize / transform.localScale.x;
+    }
+
+    private SphereCollider GetTrigger()
+    {
+        if (trigger != null) return trigger;
+        trigger = GetComponents<SphereCollider>().Where(v => v.isTrigger).First();
+        return trigger;
     }
 }
