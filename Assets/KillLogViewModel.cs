8c6b6f695485eb5618b8c6545486dc2a37b2d1ed
山本 一貴 <syhkn915@ybb.ne.jp>
2017-09-09 22:00:24 +0900

    KillLogViewModelの追加．

---
+++ Assets/KillLogViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class KillLogViewModel : MonoBehaviour {
+
+    [SerializeField] Text textUI;
+
+/// <summary>
+/// キルログにログを追記します
+/// </summary>
+/// <param name="logText">追記する文字列</param>
+    private void appendLogText(string logText)
+    {
+        if (string.IsNullOrEmpty(logText))
+        {
+            return;
+        }
+        textUI.text += logText + "\n";
+    }
+
+    /// <summary>
+    /// キルしたときに表示されるキルログを設定します
+    /// </summary>
+    /// <param name="winner">キルしたプレイヤー</param>
+    /// <param name="loser">キルされたプレイヤー</param>
+    private void playerKillLog(string winner, string loser)
+    {
+        appendLogText(winner + " が " + loser + " を キル しました");
+    }
+}
