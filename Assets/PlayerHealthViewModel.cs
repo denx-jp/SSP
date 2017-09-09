a62106fb741f74b8bc2849016925b62456244db1
山本 一貴 <syhkn915@ybb.ne.jp>
2017-09-09 19:33:01 +0900

    PlayerHealthViewModelの追加

---
+++ Assets/PlayerHealthViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerHealthViewModel : MonoBehaviour {
+
+    [SerializeField] Text uiHealthText;
+    [SerializeField] GameObject player;
+
+    private PlayerHealthManager playerHealthManager;
+
+    void Start()
+    {
+        playerHealthManager = player.GetComponent<PlayerHealthManager>();
+    }
+
+    void Update()
+    {
+        uiHealthText.text = playerHealthManager.GetComponent<IHealth>().GetHealth().ToString();
+    }
+}
