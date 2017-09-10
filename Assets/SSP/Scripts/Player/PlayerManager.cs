4eb44b42a2116d716d4f01629b07d872c64a9409
chromee <odk.mmmm@gmail.com>
2017-09-11 00:12:46 +0900

    PlayerManager作成

---
+++ Assets/SSP/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public PlayerModel playerModel;
+    public PlayerHealthManager playerHealthManager;
+    public PlayerEtherManager playerEtherManager;
+    public PlayerInputManager playerInputManager;
+
+}
