ca3061eba844f5b9dd509c5bd56d013f6b39e487
NB29979 <syhkn915@ybb.ne.jp>
2017-09-11 23:16:35 +0900

    PlayerModel，PlayerEtherManagerからHealthとEther量をとるようにリファクタリングした

--- Assets/SSP/Scripts/Player/UI/EtherViewModel.cs
+++ Assets/SSP/Scripts/Player/UI/EtherViewModel.cs
@@ -7,12 +7,10 @@ public class EtherViewModel : MonoBehaviour
 {
 
     [SerializeField] private Slider sliderEther;
-    [SerializeField] private GameObject player;
-    private PlayerEtherManager etherStream;
+    [SerializeField] private PlayerEtherManager etherStream;
 
     void Start()
     {
-        etherStream = player.GetComponent<PlayerEtherManager>();
         sliderEther.maxValue = etherStream.GetEther();
         etherStream.GetEtherStream().Subscribe(v => sliderEther.value = v);
     }
--- Assets/SSP/Scripts/Player/UI/HealthViewModel.cs
+++ Assets/SSP/Scripts/Player/UI/HealthViewModel.cs
@@ -7,12 +7,10 @@ public class HealthViewModel : MonoBehaviour
 {
 
     [SerializeField] private Slider sliderHealth;
-    [SerializeField] private GameObject player;
-    private PlayerModel healthStream;
+    [SerializeField] private PlayerModel healthStream;
 
     void Start()
     {
-        healthStream = player.GetComponent<PlayerModel>();
         sliderHealth.maxValue = healthStream.GetHealth();
         healthStream.GetHealthStream().Subscribe(v => sliderHealth.value = v);
     }
