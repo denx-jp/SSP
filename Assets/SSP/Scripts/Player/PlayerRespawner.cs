10e705a9563b4e854dc77d64d5a145b066fb6f25
oka-no <buq0045@mail4.doshisha.ac.jp>
2017-09-13 21:14:42 +0900

    コード整形

--- Assets/SSP/Scripts/Player/PlayerRespawner.cs
+++ Assets/SSP/Scripts/Player/PlayerRespawner.cs
@@ -7,28 +7,28 @@ using UniRx.Triggers;
 
 public class PlayerRespawner : MonoBehaviour
 {
-	private PlayerHealthManager playerHealthManager;
+    private PlayerHealthManager playerHealthManager;
     private PlayerModel playerModel;
 
     [SerializeField] private int timeToRespawn;
     [SerializeField] private GameObject respawnPoint;
 
-	private Animator animator;
-	private int deathHash = Animator.StringToHash("Death");
+    private Animator animator;
+    private int deathHash = Animator.StringToHash("Death");
 
     void Start()
     {
         playerModel = GetComponent<PlayerModel>();
         playerHealthManager = GetComponent<PlayerHealthManager>();
-		animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
 
-		this.playerHealthManager.GetDeathStream ()
-			.Throttle (TimeSpan.FromSeconds (timeToRespawn))
-            .Where (_ => !playerHealthManager.IsAlive ())
-            .First ()
-            .Subscribe (_ => {
-				animator.SetBool(deathHash, false);
-				this.transform.position=respawnPoint.transform.position;
-			});
+        this.playerHealthManager.GetDeathStream()
+            .Throttle(TimeSpan.FromSeconds(timeToRespawn))
+            .Where(v => v)
+            .Subscribe(_ =>
+            {
+                animator.SetBool(deathHash, false);
+                this.transform.position = respawnPoint.transform.position;
+            });
     }
 }
