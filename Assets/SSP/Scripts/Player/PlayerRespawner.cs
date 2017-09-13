8c9e7a5eeaf7ada6c889be3bc1a6658b887ee9ff
chromee <odk.mmmm@gmail.com>
2017-09-13 13:32:20 +0900

    コード整形修正

--- Assets/SSP/Scripts/Player/PlayerRespawner.cs
+++ Assets/SSP/Scripts/Player/PlayerRespawner.cs
@@ -5,45 +5,48 @@ using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 
-public class PlayerRespawner : MonoBehaviour {
+public class PlayerRespawner : MonoBehaviour
+{
 
-	private PlayerHealthManager playerHealthManager;
-	private PlayerModel playerModel;
+    private PlayerHealthManager playerHealthManager;
+    private PlayerModel playerModel;
 
-	// リスポーンするまでの時間
-	[SerializeField]
-	private int timeToRespawn;
-	// スポナー
-	[SerializeField]
-	private GameObject respawner;
+    // リスポーンするまでの時間
+    [SerializeField] private int timeToRespawn;
+    // スポナー
+    [SerializeField] private GameObject respawner;
 
-	// Use this for initialization
-	void Start () {
-		playerModel = GetComponent<PlayerModel>();
-		playerHealthManager = GetComponent<PlayerHealthManager> ();
+    // Use this for initialization
+    void Start()
+    {
+        playerModel = GetComponent<PlayerModel>();
+        playerHealthManager = GetComponent<PlayerHealthManager>();
 
-		this.UpdateAsObservable ()
-			.Where (_ => !playerHealthManager.IsAlive())
-			.First()
-			.Subscribe (_ => {
-				PlayerRespawn();
-//				RespawnTimer();
-		});
-	}
+        this.UpdateAsObservable()
+            .Where(_ => !playerHealthManager.IsAlive())
+            .First()
+            .Subscribe(_ =>
+            {
+                PlayerRespawn();
+                //				RespawnTimer();
+            });
+    }
 
-	void PlayerRespawn(){
-		Observable.Timer (TimeSpan.FromSeconds(timeToRespawn))
-			.Subscribe (_ => {
-				this.transform.position=respawner.transform.position;
-		});
-	}
+    void PlayerRespawn()
+    {
+        Observable.Timer(TimeSpan.FromSeconds(timeToRespawn))
+            .Subscribe(_ =>
+            {
+                this.transform.position = respawner.transform.position;
+            });
+    }
 
-//	void RespawnTimer(){
-//		Observable.Timer (TimeSpan.Zero, TimeSpan.FromSeconds (1))
-//			.Select (x => timeToRespawn - x)
-//			.TakeWhile (x => x > 0)
-//			.Subscribe (l => {
-//			Debug.Log ("リスポーンまであと" + l + "秒");
-//		});
-//	}
+    //	void RespawnTimer(){
+    //		Observable.Timer (TimeSpan.Zero, TimeSpan.FromSeconds (1))
+    //			.Select (x => timeToRespawn - x)
+    //			.TakeWhile (x => x > 0)
+    //			.Subscribe (l => {
+    //			Debug.Log ("リスポーンまであと" + l + "秒");
+    //		});
+    //	}
 }
