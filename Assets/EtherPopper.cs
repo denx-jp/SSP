afdb4149c1b80ae29f03797043c75997455f23ae
Becnick <yanu6sg17@yahoo.co.jp>
2017-09-14 22:26:48 +0900

    前回に対し微修正

--- Assets/EtherPopper.cs
+++ Assets/EtherPopper.cs
@@ -6,22 +6,20 @@ public class EtherPopper : MonoBehaviour {
 
 	float delta; //時間経過
 	public GameObject Ether;
+	public GameObject Point;
 	[SerializeField] private float TimeSpan; // pop間の時間設定
-	[SerializeField] private List<GameObject> Area = new List<GameObject>();	//ポップ地点の管理
+	[SerializeField] private List<GameObject> popPoints = new List<GameObject>();	//ポップ地点の管理
+
 	void Start () {
-		
+		Point = popPoints.GetRange(0, popPoints.Count);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		this.delta += Time.deltaTime;
 		if (this.delta > this.TimeSpan) {
 			this.delta = 0;
 			GameObject ether = Instantiate (Ether) as GameObject;
-			float px = Random.Range (-1, 1);
-			float py = Random.Range (-1, 1);
-			float pz = Random.Range (-1, 1);
-			ether.transform.position = new Vector3 (px, py, pz);
+			ether.transform.position = Point.transform.position;
 		}
 	}
 }
