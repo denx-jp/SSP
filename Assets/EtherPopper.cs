2c2a5d52aaa5bceff07ab310e160246799a6f981
Becnick <yanu6sg17@yahoo.co.jp>
2017-09-15 21:28:40 +0900

    EtherPopper.csをuniRxを用いたものに変更

--- Assets/EtherPopper.cs
+++ Assets/EtherPopper.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UniRx;
+using UniRx.Triggers;
+using System;
 public class EtherPopper : MonoBehaviour {
 
-	float delta; //時間経過
+	//float delta; //時間経過
 	public GameObject Ether;
 	public GameObject Point;
-	[SerializeField] private float TimeSpan; // pop間の時間設定
+	[SerializeField] private float Span; // pop間の時間設定
 	[SerializeField] private List<GameObject> popPoints = new List<GameObject>();	//ポップ地点の管理
 
 	void Start () {
-		
+		Observable.Interval(TimeSpan.FromSeconds(Span)).Subscribe(l => 
+			{
+				GameObject ether = Instantiate (Ether) as GameObject;
+				Point = popPoints [UnityEngine.Random.Range (0, popPoints.Count)]; 
+				ether.transform.position = Point.transform.position;
+		}).AddTo (this);
+	
 	}
 
 	void Update () {
-		this.delta += Time.deltaTime;
-		if (this.delta > this.TimeSpan) {
-			this.delta = 0;
-			GameObject ether = Instantiate (Ether) as GameObject;
-			Point = popPoints [Random.Range (0, popPoints.Count)]; 
-			ether.transform.position = Point.transform.position;
-		}
+		//this
+		//if (this.delta > this.Span) {
+		//	this.delta = 0;
+		//
+		//}
 	}
 }
