95d954b7b5336eb70335756e3bc6882c9674d811
chromee <odk.mmmm@gmail.com>
2017-09-18 16:56:23 +0900

    PlayerBattleUIManagerからTimeVMのTimeManagerを設定するように

--- Assets/SSP/Scripts/Player/UI/TimeViewModel.cs
+++ Assets/SSP/Scripts/Player/UI/TimeViewModel.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 
-public class TimeViewModel : MonoBehaviour {
-
+public class TimeViewModel : MonoBehaviour
+{
     [SerializeField] private Text textTime;
-    [SerializeField] private TimeManager timeManager;
+    private TimeManager timeManager;
 
     private string minutes, seconds;
 
-    void Start() {
+    void Start()
+    {
         timeManager
             .GetTimeStream()
-            .Subscribe(time => {
+            .Subscribe(time =>
+            {
                 minutes = Mathf.Ceil(time / 60).ToString("00");
                 seconds = Mathf.Ceil(time % 60).ToString("00");
                 textTime.text = minutes + ":" + seconds;
             });
+    }
+
+    public void SetTimeManager(TimeManager tm)
+    {
+        timeManager = tm;
     }
 }
