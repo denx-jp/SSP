51e0afa02676e7b9abe577558fe4ad56eb71a2a7
chromee <odk.mmmm@gmail.com>
2017-09-18 17:18:13 +0900

    ClientPlayersManager作成

---
+++ Assets/SSP/Scripts/GameManager/ClientPlayersManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientPlayersManager : MonoBehaviour
+{
+    [SerializeField] private List<PlayerManager> playerManagers;
+
+    public void AddPlayer(PlayerManager pm)
+    {
+        playerManagers.Add(pm);
+    }
+}
