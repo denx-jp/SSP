b9e5abab3c726590b1af1082da9eeb5fd2973a37
oka-no <buq0045@mail4.doshisha.ac.jp>
2017-09-20 12:50:24 +0900

    LSSがエーテルを放出するようにした

--- Assets/SSP/Scripts/LifeSupportSystem/LifeSupportSystemEtherManager.cs
+++ Assets/SSP/Scripts/LifeSupportSystem/LifeSupportSystemEtherManager.cs
@@ -10,6 +10,14 @@ public class LifeSupportSystemEtherManager : MonoBehaviour, IInteractable
     [SerializeField] private float initEther;
     [SerializeField] private float etherReductionRate;
     [SerializeField] private float etherChargeValue;
+
+    [SerializeField] private GameObject playerBat;
+    [SerializeField] private GameObject etherObject;
+    [SerializeField] private float LSSPopEther;
+    [SerializeField] private float emitPower;
+
+    [SerializeField] private WeaponAttacker weaponAttacker;
+
     private LifeSupportSystemModel lifeSupportSystemModel;
 
     void Start()
@@ -22,6 +30,15 @@ public class LifeSupportSystemEtherManager : MonoBehaviour, IInteractable
             {
                 ReduceEther(etherReductionRate);
             }).AddTo(this);
+
+        gameObject.OnTriggerEnterAsObservable()
+            .Where(v => v.gameObject == playerBat)
+            .Where(l => weaponAttacker.isAttackStarted)
+            .Subscribe(_ =>
+            {
+                ReduceEther(LSSPopEther);
+                GenerateEtherObject(LSSPopEther);
+            });
     }
 
     private void ReduceEther(float ether)
@@ -38,5 +55,24 @@ public class LifeSupportSystemEtherManager : MonoBehaviour, IInteractable
     {
         playerManager.playerEtherManager.EmitEther(etherChargeValue);
         AcquireEther(etherChargeValue);
+    }
+
+    private void GenerateEtherObject(float emitEtherValue){
+        float LSSemithigh = 0.0f;
+        var singleEtherValue = emitEtherValue / 10;
+        while (emitEtherValue > 0)
+        {
+            var emittedEtherObject = Instantiate(etherObject, transform.position + Vector3.up * LSSemithigh, transform.rotation);
+
+            if (emitEtherValue < singleEtherValue) singleEtherValue = emitEtherValue;
+
+            emitEtherValue -= singleEtherValue;
+            emittedEtherObject.GetComponent<EtherObject>().Init(singleEtherValue);
+            LSSemithigh += emittedEtherObject.transform.localScale.y;
+
+            var emitDirestion = Vector3.up + new Vector3(UnityEngine.Random.Range(-emitPower, emitPower), 0, UnityEngine.Random.Range(-emitPower, emitPower));
+            emittedEtherObject.GetComponent<Rigidbody>().AddForce(emitDirestion, ForceMode.Impulse);
+        }
+
     }
 }
