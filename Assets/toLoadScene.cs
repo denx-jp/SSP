8fe654a546b4219181d82a99c80eff71083c4c14
Becnick <yanu6sg17@yahoo.co.jp>
2017-09-20 13:34:57 +0900

    TitleのStartボタンを押すとBattleに遷移

---
+++ Assets/toLoadScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toLoadScene : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    public void LoadBattle()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+
+}
