250b8232493f5914e68cb3a41ee42b4d542f5900
NB29979 <syhkn915@ybb.ne.jp>
2017-09-21 00:54:29 +0900

    Ether量についての引き分けを含む判定を行えるようにした．制限時間オーバーには未対応．

--- Assets/GameJudger.cs
+++ Assets/GameJudger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
@@ -8,29 +9,26 @@ public class GameJudger : MonoBehaviour {
     [SerializeField] private LifeSupportSystemModel aTeamLSSModel;
     [SerializeField] private LifeSupportSystemModel bTeamLSSModel;
 
-    private float aTeamEther;
-    private float bTeamEther;
+    private Subject<int> judgeStream;
 
-    public Subject<int> judgeStream;
-
-	// Use this for initialization
 	void Start () {
         judgeStream = new Subject<int>();
 
-        aTeamLSSModel.ether
-            .Where(v => v <= 0)
-            .Subscribe(v => judgeStream.OnNext(2));
-
-        bTeamLSSModel.ether
-            .Where(v => v <= 0)
-            .Subscribe(v => judgeStream.OnNext(1));
+        Observable
+            .ZipLatest(aTeamLSSModel.ether, bTeamLSSModel.ether)
+            .Where(v => v[0] <= 0 || v[1] <= 0)
+            .Subscribe(v =>
+            {
+                if (v[0] > v[1]) { judgeStream.OnNext(1); }
+                else if (v[0] < v[1]) { judgeStream.OnNext(2); }
+                else if (v[0] == 0 && v[1] == 0) { judgeStream.OnNext(3); }
+            }).AddTo(this);
 
         judgeStream.Subscribe(vv => Debug.Log(vv));
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Subject<int> GetjudgeStream()
+    {
+        return judgeStream;
+    }
 }
