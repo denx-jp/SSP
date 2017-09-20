6c251ceccbcbd2bdf9d1df6dde36bd6bf1ea70db
NB29979 <syhkn915@ybb.ne.jp>
2017-09-20 23:38:23 +0900

    各チームのEther量による勝敗判定をDebugLogするようにした．引き分けは未実装．

--- Assets/GameJudger.cs
+++ Assets/GameJudger.cs
@@ -8,10 +8,25 @@ public class GameJudger : MonoBehaviour {
     [SerializeField] private LifeSupportSystemModel aTeamLSSModel;
     [SerializeField] private LifeSupportSystemModel bTeamLSSModel;
 
+    private float aTeamEther;
+    private float bTeamEther;
+
+    public Subject<int> judgeStream;
+
 	// Use this for initialization
 	void Start () {
-        aTeamLSSModel.ether.Subscribe(v=>Debug.Log("team A:"+v));
-        bTeamLSSModel.ether.Subscribe(v=>Debug.Log("team B:"+v));
+        judgeStream = new Subject<int>();
+
+        aTeamLSSModel.ether
+            .Where(v => v <= 0)
+            .Subscribe(v => judgeStream.OnNext(2));
+
+        bTeamLSSModel.ether
+            .Where(v => v <= 0)
+            .Subscribe(v => judgeStream.OnNext(1));
+
+        judgeStream.Subscribe(vv => Debug.Log(vv));
+
 	}
 	
 	// Update is called once per frame
