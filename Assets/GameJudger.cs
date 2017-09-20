b40e97e9c6fed3b04603db59fcf7242ad5444aa3
NB29979 <syhkn915@ybb.ne.jp>
2017-09-20 23:07:14 +0900

    Gameの終了判定を行うコンポーネント，GameJudgerを追加

---
+++ Assets/GameJudger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class GameJudger : MonoBehaviour {
+
+    [SerializeField] private LifeSupportSystemModel aTeamLSSModel;
+    [SerializeField] private LifeSupportSystemModel bTeamLSSModel;
+
+	// Use this for initialization
+	void Start () {
+        aTeamLSSModel.ether.Subscribe(v=>Debug.Log("team A:"+v));
+        bTeamLSSModel.ether.Subscribe(v=>Debug.Log("team B:"+v));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
