07ca6718aad829257bfa2b36a2a23d6c97d8e1f2
NB29979 <syhkn915@ybb.ne.jp>
2017-09-22 18:40:35 +0900

    シーンの調整

--- Assets/GameJudger.cs
+++ Assets/GameJudger.cs
@@ -10,18 +10,24 @@ public class GameJudger : MonoBehaviour {
     [SerializeField] private LifeSupportSystemEtherManager teamBLSSManager;
 
     private Subject<int> judgeStream;
+    private GameObject[] players;
 
 	void Start () {
         judgeStream = new Subject<int>();
+        players = GameObject.FindGameObjectsWithTag(TagMap.Player);
+
         Observable
-            .ZipLatest(teamALSSManager.GetD, bTeamLSSModel.ether)
-            .Where(v => v[0] <= 0 || v[1] <= 0)
-            .Subscribe(v =>
-            {
-                if (v[0] > v[1]) { judgeStream.OnNext(1); }
-                else if (v[0] < v[1]) { judgeStream.OnNext(2); }
-                else if (v[0] == 0 && v[1] == 0) { judgeStream.OnNext(3); }
-            }).AddTo(this);
+            //.CombineLatest(teamALSSManager, teamBLSSManager)
+
+        //Observable
+        //    .ZipLatest(teamALSSManager.ether, bTeamLSSModel.ether)
+        //    .Where(v => v[0] <= 0 || v[1] <= 0)
+        //    .Subscribe(v =>
+        //    {
+        //        if (v[0] > v[1]) { judgeStream.OnNext(1); }
+        //        else if (v[0] < v[1]) { judgeStream.OnNext(2); }
+        //        else if (v[0] == 0 && v[1] == 0) { judgeStream.OnNext(3); }
+        //    }).AddTo(this);
 
         judgeStream.Subscribe(vv => Debug.Log(vv));
 
