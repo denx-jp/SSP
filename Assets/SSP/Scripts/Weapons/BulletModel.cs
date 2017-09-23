40a5b47fe2e9acdfc52250ed5e08361b7c220861
chromee <odk.mmmm@gmail.com>
2017-09-23 21:46:56 +0900

    LongRangeWeaponもチーム判別ができるように

--- Assets/SSP/Scripts/Weapons/BulletModel.cs
+++ Assets/SSP/Scripts/Weapons/BulletModel.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class BulletModel : MonoBehaviour
 {
-    public int shootPlayerId;
+    public int shootPlayerId, shootPlayerTeamId;
     public float damageAmount, deathTime;
 
-    public void SetProperties(int pId, float dAmount, float dTime)
+    public void SetProperties(int _shootPlayerId, int _shootPlayerTeamId, float _damageAmount, float _deathTime)
     {
-        shootPlayerId = pId;
-        damageAmount = dAmount;
-        deathTime = dTime;
+        shootPlayerId = _shootPlayerId;
+        shootPlayerTeamId = _shootPlayerTeamId;
+        damageAmount = _damageAmount;
+        deathTime = _deathTime;
     }
 }
