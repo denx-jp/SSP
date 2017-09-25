58730c09548c146901737c2c9ef0d9002d7ac2c3
chromee <odk.mmmm@gmail.com>
2017-09-25 12:22:02 +0900

    勝敗判定をGameManager側で行うように

--- Assets/GameJudger.cs
+++ Assets/GameJudger.cs
@@ -5,19 +5,15 @@ using System.Linq;
 using UnityEngine;
 using UniRx;
 
-public class GameJudger : MonoBehaviour {
-
+public class GameJudger : MonoBehaviour
+{
+    [SerializeField] private ClientPlayersManager clientPlayersManager;
     [SerializeField] private LifeSupportSystemEtherManager team1_LSSManager;
     [SerializeField] private LifeSupportSystemEtherManager team2_LSSManager;
+    
+    private int localPlayerTeamId;
 
-    private Subject<int> winnerStream;
-
-    void Awake()
-    {
-        winnerStream = new Subject<int>();
-    }
-
-	void Start ()
+    void Start()
     {
         Observable
             .Merge(team1_LSSManager.GetDeathStream(), team2_LSSManager.GetDeathStream())
@@ -25,11 +21,21 @@ public class GameJudger : MonoBehaviour {
             {
                 // 敗北したチームのidを勝利したチームのidに変換
                 // v = 1 -> 2, v = 2 -> 1
-                winnerStream.OnNext(v * 2 % 3);
+                int winnerTeamId = v * 2 % 3;
+
+                PlayerManager localPlayerManager = clientPlayersManager.playerManagers.Find(p => p.gameObject.layer == LayerMap.LocalPlayer);
+                int localPlayerId = localPlayerManager.playerModel.teamId;
+
+                if (winnerTeamId==localPlayerTeamId)
+                {
+                    Debug.Log("Player" + localPlayerManager.playerModel.playerId +
+                                "(Team" + localPlayerManager.playerModel.teamId + ")" + " :勝利");
+                }
+                else
+                {
+                    Debug.Log("Player" + localPlayerManager.playerModel.playerId +
+                                "(Team" + localPlayerManager.playerModel.teamId + ")" + " :敗北");
+                }
             });
-    }
-    public Subject<int> GetWinnerStream()
-    {
-        return winnerStream;
     }
 }
