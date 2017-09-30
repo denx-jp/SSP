f950ca5f464e49c4ec30d98bafe6c4292e8a090f
Becnick <yanu6sg17@yahoo.co.jp>
2017-09-30 15:15:08 +0900

    LSSより一定の距離以上をもつリスポーン地点を削除

--- Assets/PlayerChooseRespawnPoints.cs
+++ Assets/PlayerChooseRespawnPoints.cs
@@ -7,24 +7,47 @@ using System.Linq;
 
 public class PlayerChooseRespawnPoints : MonoBehaviour {
 
+    public GameObject LSS;
     private PlayerInputManager pim;
     [SerializeField] private List<GameObject> RespawnPoints = new List<GameObject>();
-    void Start () {
+    [SerializeField] private float Range;
 
+    void Start () {
         pim = GetComponent<PlayerInputManager>();
+
+        for (int i = 0; i < RespawnPoints.Count; i++)
+        {
+            float Distance = (LSS.transform.position - RespawnPoints[i].transform.position).sqrMagnitude;
+            // LSSとリスポーン地点の二点間の距離をとる
+            if (Distance > Range)
+            {
+                RespawnPoints.RemoveAt(i);
+            }
+        }
         pim.ChooseRespawnPointsButtonDown
             .Where(v => v)
             .Where(v => Input.GetButtonDown("Right"))
             .Subscribe(v =>
             {
-                
+                 
             });
         pim.ChooseRespawnPointsButtonDown
             .Where(v => v)
             .Where(v => Input.GetButtonDown("Left"))
             .Subscribe(v =>
             {
-
+                //実行内容
             });
+    }
+    void Update()
+    {
+        //for (int i = 0; i < RespawnPoints.Count ; i++){
+        //    float Distance = (LSS.transform.position - RespawnPoints[i].transform.position).sqrMagnitude;
+        //    // LSSとリスポーン地点の二点間の距離をとる
+        //   if(Distance > Range)
+        //    {
+        //        RespawnPoints.RemoveAt(i);
+        //    }
+        //}
     }
 }
