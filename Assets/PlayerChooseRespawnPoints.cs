44b37079118d68fcceef060e1b5f275075ed51d5
Becnick <yanu6sg17@yahoo.co.jp>
2017-09-30 14:00:08 +0900

    Respawnに関するボタンまわりのコード作成

---
+++ Assets/PlayerChooseRespawnPoints.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System.Linq;
+
+public class PlayerChooseRespawnPoints : MonoBehaviour {
+
+    private PlayerInputManager pim;
+    [SerializeField] private List<GameObject> RespawnPoints = new List<GameObject>();
+    void Start () {
+
+        pim = GetComponent<PlayerInputManager>();
+        pim.ChooseRespawnPointsButtonDown
+            .Where(v => v)
+            .Where(v => Input.GetButtonDown("Right"))
+            .Subscribe(v =>
+            {
+                
+            });
+        pim.ChooseRespawnPointsButtonDown
+            .Where(v => v)
+            .Where(v => Input.GetButtonDown("Left"))
+            .Subscribe(v =>
+            {
+
+            });
+    }
+}
