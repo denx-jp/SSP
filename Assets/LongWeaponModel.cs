1fc98d8db0e8aeb5df93240124aa97181d279c2f
frontvent <kitamoto0302@docomo.ne.jp>
2017-10-07 22:14:43 +0900

    WeaponModelの作成

---
+++ Assets/LongWeaponModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongWeaponModel : MonoBehaviour {
+    public string weaponName ;
+    private BulletModel bulletModel;
+    private int playerId, teamId;
+
+    public void WeaponModel(PlayerModel playerModel)
+    {
+        playerId = playerModel.playerId;
+        teamId = playerModel.teamId;
+    }
+
+}
---
+++ Assets/ShortWeaponModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortWeaponModel : MonoBehaviour
+{
+    public string weaponName;
+    private int playerId, teamId;
+
+    public void WeaponModel(PlayerModel playerModel)
+    {
+        playerId = playerModel.playerId;
+        teamId = playerModel.teamId;
+    }
+}
