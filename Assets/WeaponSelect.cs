0ae36a22dfc15ddc5c0a46f765233520654ac5b4
frontvent <kitamoto0302@docomo.ne.jp>
2017-10-07 22:18:47 +0900

    インベントリ内での武器選択用UIの作成

---
+++ Assets/WeaponSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelect : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        //F（左）とG（右）で武器選択
+        if ((Input.GetKeyDown(KeyCode.F)) && (this.transform.position.x >= 50)) 
+        {
+            this.transform.position += new Vector3(-46.3f, 0, 0);
+        }
+        if ((Input.GetKeyDown(KeyCode.G))&& (this.transform.position.x <=170)) 
+        {
+            this.transform.position += new Vector3(46.3f, 0, 0);
+        }
+    }
+}
