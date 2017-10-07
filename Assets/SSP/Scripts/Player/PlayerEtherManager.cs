bd607cbe6ea7fe9402dc05c7e52c98991bdfb9c9
NB29979 <syhkn915@ybb.ne.jp>
2017-10-08 00:10:33 +0900

    Etherのinstantiate位置，放出角度の同期ができるように

--- Assets/SSP/Scripts/Player/PlayerEtherManager.cs
+++ Assets/SSP/Scripts/Player/PlayerEtherManager.cs
@@ -12,10 +12,26 @@ public class PlayerEtherManager : NetworkBehaviour, IEtherAcquirer, IEtherEmitte
     private PlayerModel playerModel;
     private PlayerHealthManager playerHealthManager;
 
+    private struct PlayerEtherProp{
+        public float value;
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 direction;
+
+        public PlayerEtherProp(float _value, Vector3 _position, Quaternion _rotation, Vector3 _direction)
+        {
+            value = _value; position = _position;
+            rotation = _rotation; direction = _direction;
+        }
+    }
+    private class PlayerEtherProps : SyncListStruct<PlayerEtherProp>{ };
+    private PlayerEtherProps playerEtherProps;
+
     private void Start()
     {
         playerModel = GetComponent<PlayerModel>();
         playerHealthManager = GetComponent<PlayerHealthManager>();
+        playerEtherProps = new PlayerEtherProps();
 
         playerHealthManager.GetDeathStream()
              .Where(v => v)
@@ -30,6 +46,10 @@ public class PlayerEtherManager : NetworkBehaviour, IEtherAcquirer, IEtherEmitte
 #endif
     private void CmdStartPlayerEtherPop()
     {
+        var halfEther = playerModel.Ether.Value / 2.0f;
+        EmitEther(halfEther);
+        GenerateEtherObjectProp(halfEther);
+
         RpcStartPlayerEtherPop();
     }
 #if ONLINE
@@ -37,29 +57,32 @@ public class PlayerEtherManager : NetworkBehaviour, IEtherAcquirer, IEtherEmitte
 #endif
     private void RpcStartPlayerEtherPop()
     {
-        var halfEther = playerModel.Ether.Value / 2.0f;
-        EmitEther(halfEther);
-        GenerateEtherObject(halfEther);
+        float emithigh = 0;
+        foreach(var etherProp in playerEtherProps)
+        {
+            var emittedEtherObject = Instantiate(etherObject, etherProp.position + Vector3.up * emithigh, etherProp.rotation);
+            emittedEtherObject.GetComponent<EtherObject>().Init(etherProp.value);
+            emithigh += emittedEtherObject.transform.localScale.y;
+            emittedEtherObject.GetComponent<Rigidbody>().AddForce(etherProp.direction, ForceMode.Impulse);
+        }
     }
 
     //非常に雑な実装なので治せるなら後から治した方がよい
-    private void GenerateEtherObject(float emitEtherValue)
+    private void GenerateEtherObjectProp(float emitEtherValue)
     {
-        float emithigh = 0;
-        var singleEtherValue = emitEtherValue / 10;
+        float singleEtherValue = emitEtherValue / 10;
         while (emitEtherValue > 0)
         {
-            var emittedEtherObject = Instantiate(etherObject, transform.position + Vector3.up * emithigh, transform.rotation);
-
             if (emitEtherValue < singleEtherValue)
                 singleEtherValue = emitEtherValue;
             emitEtherValue -= singleEtherValue;
-            emittedEtherObject.GetComponent<EtherObject>().Init(singleEtherValue);
-            emithigh += emittedEtherObject.transform.localScale.y;      //SetEtherでemittedEtherObjectのサイズが変更されることに依存する
 
-            var emitDirection = Vector3.up + new Vector3(Random.Range(-emitPower, emitPower), 0, Random.Range(-emitPower, emitPower));
-            emittedEtherObject.GetComponent<Rigidbody>().AddForce(emitDirection, ForceMode.Impulse);
-
+            playerEtherProps.Add(new PlayerEtherProp(
+                    singleEtherValue,
+                    transform.position,
+                    transform.rotation,
+                    (Vector3.up + new Vector3(Random.Range(-emitPower, emitPower), 0, Random.Range(-emitPower, emitPower))))
+                );
         }
     }
 
