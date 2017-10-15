6ed29e555abf0e833d97cd55bd4e9752cef61f7c
chromee <odk.mmmm@gmail.com>
2017-10-15 18:11:43 +0900

    武器を構えていない状態で歩けるように

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+//移動やジャンプにおけるTransform・RigidBodyの操作など
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] private Rigidbody rigid;
+
+    [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float dashSpeed = 6f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float jumpPower = 6f;
+    [SerializeField] private float groundCheckDistance = 1f;
+
+    private bool isOnGround = true;
+    private Vector3 groundNormal;
+    private float currentGroundCheckDistance;
+    private bool isHoldWeapon = false;
+
+    private void Start()
+    {
+        currentGroundCheckDistance = groundCheckDistance;
+    }
+
+    public void Move(Vector3 move, bool isDash)
+    {
+        if (move.magnitude > 1f) move.Normalize();
+        var speed = isDash ? dashSpeed : walkSpeed;
+        move = Vector3.ProjectOnPlane(move, groundNormal) * speed;
+        rigid.velocity = move;
+
+        if (move != Vector3.zero)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), Time.deltaTime * rotationSpeed);
+
+        UpdateAnimator(move);
+    }
+
+    void UpdateAnimator(Vector3 move)
+    {
+        var forwardVal = move.magnitude / dashSpeed;
+        animator.SetFloat("Move Z", forwardVal, 0.1f, Time.deltaTime);
+        animator.SetBool("OnGround", isOnGround);
+        if (!isOnGround) animator.SetFloat("Move Y", rigid.velocity.y);
+    }
+
+    #region MyRegion
+    //public void Jump()
+    //{
+    //    rigid.velocity = new Vector3(rigid.velocity.x, jumpPower, rigid.velocity.z);
+    //    isOnGround = false;
+    //    currentGroundCheckDistance = 0.1f;
+    //}
+
+    //private void Land()
+    //{
+    //    isOnGround = true;
+    //    currentGroundCheckDistance = groundCheckDistance;
+    //}
+
+    private void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position + Vector3.up * 0.1f, Vector3.down, out hitInfo, currentGroundCheckDistance))
+        {
+            isOnGround = true;
+            groundNormal = hitInfo.normal;
+        }
+        else
+        {
+            isOnGround = false;
+            groundNormal = Vector3.up;
+        }
+    }
+    #endregion
+}
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -16,9 +16,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		[SerializeField] float m_AnimSpeedMultiplier = 1f;
 		[SerializeField] float m_GroundCheckDistance = 0.1f;
 
-		Rigidbody m_Rigidbody;
-		Animator m_Animator;
-		bool m_IsGrounded;
+		Rigidbody rigid;
+		Animator animator;
+		bool isOnGround;
 		float m_OrigGroundCheckDistance;
 		const float k_Half = 0.5f;
 		float m_TurnAmount;
@@ -27,18 +27,18 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		float m_CapsuleHeight;
 		Vector3 m_CapsuleCenter;
 		CapsuleCollider m_Capsule;
-		bool m_Crouching;
+		bool isCrouching;
 
 
 		void Start()
 		{
-			m_Animator = GetComponent<Animator>();
-			m_Rigidbody = GetComponent<Rigidbody>();
+			animator = GetComponent<Animator>();
+			rigid = GetComponent<Rigidbody>();
 			m_Capsule = GetComponent<CapsuleCollider>();
 			m_CapsuleHeight = m_Capsule.height;
 			m_CapsuleCenter = m_Capsule.center;
 
-			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+			rigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 			m_OrigGroundCheckDistance = m_GroundCheckDistance;
 		}
 
@@ -51,7 +51,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			// direction.
 			if (move.magnitude > 1f) move.Normalize();
 			move = transform.InverseTransformDirection(move);
+
 			CheckGroundStatus();
+
 			move = Vector3.ProjectOnPlane(move, m_GroundNormal);
 			m_TurnAmount = Mathf.Atan2(move.x, move.z);
 			m_ForwardAmount = move.z;
@@ -59,7 +61,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			ApplyExtraTurnRotation();
 
 			// control and velocity handling is different when grounded and airborne:
-			if (m_IsGrounded)
+			if (isOnGround)
 			{
 				HandleGroundedMovement(crouch, jump);
 			}
@@ -68,48 +70,47 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 				HandleAirborneMovement();
 			}
 
-			ScaleCapsuleForCrouching(crouch);
-			PreventStandingInLowHeadroom();
-
-			// send input and other state parameters to the animator
+			//ScaleCapsuleForCrouching(crouch);
+			//PreventStandingInLowHeadroom();
+            
 			UpdateAnimator(move);
 		}
 
 
 		void ScaleCapsuleForCrouching(bool crouch)
 		{
-			if (m_IsGrounded && crouch)
+			if (isOnGround && crouch)
 			{
-				if (m_Crouching) return;
+				if (isCrouching) return;
 				m_Capsule.height = m_Capsule.height / 2f;
 				m_Capsule.center = m_Capsule.center / 2f;
-				m_Crouching = true;
+				isCrouching = true;
 			}
 			else
 			{
-				Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
+				Ray crouchRay = new Ray(rigid.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
 				float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
 				if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
 				{
-					m_Crouching = true;
+					isCrouching = true;
 					return;
 				}
 				m_Capsule.height = m_CapsuleHeight;
 				m_Capsule.center = m_CapsuleCenter;
-				m_Crouching = false;
+				isCrouching = false;
 			}
 		}
 
 		void PreventStandingInLowHeadroom()
 		{
 			// prevent standing up in crouch-only zones
-			if (!m_Crouching)
+			if (!isCrouching)
 			{
-				Ray crouchRay = new Ray(m_Rigidbody.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
+				Ray crouchRay = new Ray(rigid.position + Vector3.up * m_Capsule.radius * k_Half, Vector3.up);
 				float crouchRayLength = m_CapsuleHeight - m_Capsule.radius * k_Half;
 				if (Physics.SphereCast(crouchRay, m_Capsule.radius * k_Half, crouchRayLength, Physics.AllLayers, QueryTriggerInteraction.Ignore))
 				{
-					m_Crouching = true;
+					isCrouching = true;
 				}
 			}
 		}
@@ -118,13 +119,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		void UpdateAnimator(Vector3 move)
 		{
 			// update the animator parameters
-			m_Animator.SetFloat("Forward", m_ForwardAmount, 0.1f, Time.deltaTime);
-			m_Animator.SetFloat("Turn", m_TurnAmount, 0.1f, Time.deltaTime);
-			m_Animator.SetBool("Crouch", m_Crouching);
-			m_Animator.SetBool("OnGround", m_IsGrounded);
-			if (!m_IsGrounded)
+			animator.SetFloat("Move Z", m_ForwardAmount, 0.1f, Time.deltaTime);
+			animator.SetFloat("Move X", m_TurnAmount, 0.1f, Time.deltaTime);
+			animator.SetBool("Crouch", isCrouching);
+			animator.SetBool("OnGround", isOnGround);
+			if (!isOnGround)
 			{
-				m_Animator.SetFloat("Jump", m_Rigidbody.velocity.y);
+				animator.SetFloat("Jump", rigid.velocity.y);
 			}
 
 			// calculate which leg is behind, so as to leave that leg trailing in the jump animation
@@ -132,23 +133,23 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			// and assumes one leg passes the other at the normalized clip times of 0.0 and 0.5)
 			float runCycle =
 				Mathf.Repeat(
-					m_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
+					animator.GetCurrentAnimatorStateInfo(0).normalizedTime + m_RunCycleLegOffset, 1);
 			float jumpLeg = (runCycle < k_Half ? 1 : -1) * m_ForwardAmount;
-			if (m_IsGrounded)
+			if (isOnGround)
 			{
-				m_Animator.SetFloat("JumpLeg", jumpLeg);
+				animator.SetFloat("JumpLeg", jumpLeg);
 			}
 
 			// the anim speed multiplier allows the overall speed of walking/running to be tweaked in the inspector,
 			// which affects the movement speed because of the root motion.
-			if (m_IsGrounded && move.magnitude > 0)
+			if (isOnGround && move.magnitude > 0)
 			{
-				m_Animator.speed = m_AnimSpeedMultiplier;
+				animator.speed = m_AnimSpeedMultiplier;
 			}
 			else
 			{
 				// don't use that while airborne
-				m_Animator.speed = 1;
+				animator.speed = 1;
 			}
 		}
 
@@ -157,21 +158,21 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		{
 			// apply extra gravity from multiplier:
 			Vector3 extraGravityForce = (Physics.gravity * m_GravityMultiplier) - Physics.gravity;
-			m_Rigidbody.AddForce(extraGravityForce);
+			rigid.AddForce(extraGravityForce);
 
-			m_GroundCheckDistance = m_Rigidbody.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
+			m_GroundCheckDistance = rigid.velocity.y < 0 ? m_OrigGroundCheckDistance : 0.01f;
 		}
 
 
 		void HandleGroundedMovement(bool crouch, bool jump)
 		{
 			// check whether conditions are right to allow a jump:
-			if (jump && !crouch && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
+			if (jump && !crouch && animator.GetCurrentAnimatorStateInfo(0).IsName("Grounded"))
 			{
 				// jump!
-				m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpPower, m_Rigidbody.velocity.z);
-				m_IsGrounded = false;
-				m_Animator.applyRootMotion = false;
+				rigid.velocity = new Vector3(rigid.velocity.x, m_JumpPower, rigid.velocity.z);
+				isOnGround = false;
+				animator.applyRootMotion = false;
 				m_GroundCheckDistance = 0.1f;
 			}
 		}
@@ -188,13 +189,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 		{
 			// we implement this function to override the default root motion.
 			// this allows us to modify the positional speed before it's applied.
-			if (m_IsGrounded && Time.deltaTime > 0)
+			if (isOnGround && Time.deltaTime > 0)
 			{
-				Vector3 v = (m_Animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
+				Vector3 v = (animator.deltaPosition * m_MoveSpeedMultiplier) / Time.deltaTime;
 
 				// we preserve the existing y part of the current velocity.
-				v.y = m_Rigidbody.velocity.y;
-				m_Rigidbody.velocity = v;
+				v.y = rigid.velocity.y;
+				rigid.velocity = v;
 			}
 		}
 
@@ -211,14 +212,14 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 			if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, m_GroundCheckDistance))
 			{
 				m_GroundNormal = hitInfo.normal;
-				m_IsGrounded = true;
-				m_Animator.applyRootMotion = true;
+				isOnGround = true;
+				animator.applyRootMotion = true;
 			}
 			else
 			{
-				m_IsGrounded = false;
+				isOnGround = false;
 				m_GroundNormal = Vector3.up;
-				m_Animator.applyRootMotion = false;
+				animator.applyRootMotion = false;
 			}
 		}
 	}
