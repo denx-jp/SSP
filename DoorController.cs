3abbcf596aa17e2fea95d56363076df26ff29525
iieiieiie <nwdm5tkiw6@gmail.com>
2017-10-22 16:00:41 +0900

    itweenを使用したコード

--- DoorController.cs
+++ DoorController.cs
@@ -5,30 +5,18 @@ using UnityEngine;
 public class DoorController : MonoBehaviour, IInteractable　//IInteractableを継承
 {
     bool isOpen; //falseが閉まっている状態、trueが開いている状態
-    GameObject dooredge1;
-    GameObject dooredge2;
-    Vector3 EdgePos1;
-    Vector3 EdgePos2;
     //Interactメソッドが呼ばれるとisOpen切り替え、trueかfalseでOpenかCloseのメソッド呼び出し
     public void Interact(PlayerManager playerManager)
     {
         isOpen = !isOpen;
-       
     }
     void Start()
     {
         isOpen = false; //最初は閉まっている
-        dooredge1 = GameObject.Find("DoorEdge1");
-        dooredge2 = GameObject.Find("DoorEdge2");
-        EdgePos1 = dooredge1.transform.position;//扉が閉まった後静止する地点
-        EdgePos2 = dooredge2.transform.position;//扉が開いた後静止する地点
-        
     }
 
     void Update()
     {
-        //扉の移動範囲をEdgePos1のx座標からEdgePos2のx座標まで制限
-        transform.position = (new Vector3(Mathf.Clamp(transform.position.x, EdgePos1.x, EdgePos2.x), transform.position.y, transform.position.z));
         if (isOpen)
         {
             Open();
@@ -41,19 +29,11 @@ public class DoorController : MonoBehaviour, IInteractable　//IInteractableを�
 
     private void Close()
     {
-        for (int i = 0; i<10; i++)
-        {
-           
-            transform.position += new Vector3(0.05f, 0.0f, 0.0f);
-        }
-
+        iTween.MoveTo(gameObject, new Vector3(10, 0, 0), 2.0f);
     }
 
     private void Open()
     {
-       for(int i=0; i<10; i++)
-        {
-            transform.position += new Vector3(-0.05f, 0.0f, 0.0f);
-        }
+        iTween.MoveTo(gameObject, new Vector3(-10, 0, 0), 2.0f);        
     }
 }
