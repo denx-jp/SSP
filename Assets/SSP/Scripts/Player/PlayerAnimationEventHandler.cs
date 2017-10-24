a64e64ddc99e1c236417a439805ff140553a85d4
RauziDssh <rauzi.univ@gmail.com>
2017-10-24 19:32:14 +0900

    init

---
+++ Assets/SSP/Scripts/Player/PlayerAnimationEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationEventHandler : MonoBehaviour {
+
+    void FootR() { }
+    void FootL() { }
+    void Hit() { }
+}
