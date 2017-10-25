57b2945da440149c7d5fcbcce85b43c17c084124
oka-no <buq0045@mail4.doshisha.ac.jp>
2017-10-25 22:18:14 +0900

    右クリックでカメラが切り替わる

---
+++ Assets/SSP/Scripts/Weapons/SwitchScope.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class SwitchScope : MonoBehaviour
+{
+
+    [SerializeField] private GameObject scopeCam;
+    [SerializeField] private GameObject cam;
+    PlayerInputManager pIM;
+
+    void Start()
+    {
+        pIM = GetComponent<PlayerInputManager>();
+        pIM.ScopeButtonDown
+           .Where(v => v)
+           .Subscribe(_ =>
+           {
+            if (scopeCam.activeInHierarchy)
+               {
+                   scopeCam.SetActive(false);
+                   cam.SetActive(true);
+               }
+               else
+               {
+                   cam.SetActive(false);
+                   scopeCam.SetActive(true);
+               }
+           });
+    }
+}
