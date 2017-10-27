b9896f486dfc37a8283da39d61dcb17c5ac9ad19
chromee <odk.mmmm@gmail.com>
2017-10-28 01:25:16 +0900

    タイトル画面修正

---
+++ Assets/SSP/Scripts/UI/ButtonSound.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+
+    [SerializeField] AudioClip sound;
+
+    public void PlayeSE()
+    {
+        AudioSource.PlayClipAtPoint(sound, Vector3.zero);
+    }
+}
