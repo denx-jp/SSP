30c40fcb273211ebc14d4d50d15c08d5fe4a3b42
chromee <odk.mmmm@gmail.com>
2017-10-28 00:13:30 +0900

    GameManager作成

---
+++ Assets/SSP/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    [SerializeField] private ClientPlayersManager clientPlayersManager;
+    [SerializeField] private TimeManager timeManager;
+    [SerializeField] private GameJudger gameJudger;
+
+    [SerializeField] private Text message;
+
+    [SerializeField] private GameObject team1LSS;
+    [SerializeField] private GameObject team2LSS;
+
+    [SerializeField] private float startDelay = 3f;
+    [SerializeField] private float endDelay = 3f;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    [ServerCallback]
+    private void Start()
+    {
+
+    }
+    
+    private void Game()
+    {
+
+    }
+}
