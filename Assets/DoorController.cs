304ae0b941fa3bca33da0c1d2b87e6515ec57a3c
iieiieiie <nwdm5tkiw6@gmail.com>
2017-10-27 21:58:16 +0900

    シーン作成

---
+++ Assets/DoorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour//, IInteractable　//IInteractableを継承
+{
+    public GameObject door;
+    bool isOpen; //falseが閉まっている状態、trueが開いている状態
+    //Interactメソッドが呼ばれるとisOpen切り替え、trueかfalseでOpenかCloseのメソッド呼び出し
+    public void Interact(/*PlayerManager playerManager*/)
+    {
+        isOpen = !isOpen;
+    }
+    void Start()
+    {
+        isOpen = false; //最初は閉まっている
+        
+    }
+
+    void Update()
+    {
+        Debug.Log(transform.position.x);
+        if (isOpen)
+        {
+            Open();
+        }
+        else if (!isOpen)
+        {
+            Close();
+        }
+    }
+
+    private void Close()
+    {
+        iTween.MoveTo(door, iTween.Hash("x", 3f));
+    }
+
+    private void Open()
+    {
+        iTween.MoveTo(door, iTween.Hash("x", -3f));
+    }
+}
