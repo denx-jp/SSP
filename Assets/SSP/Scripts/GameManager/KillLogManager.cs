e37c8cbb0d715e19589d9e788f54b4aace9686fe
RauziDssh <rauzi.univ@gmail.com>
2017-11-02 16:56:45 +0900

    キルログを記録するように

---
+++ Assets/SSP/Scripts/GameManager/KillLogManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Networking;
+using UniRx;
+
+public class KillLogManager : NetworkBehaviour
+{
+
+    private Dictionary<int, PlayerRecord> playerRecords = new Dictionary<int, PlayerRecord>();
+
+    // Use this for initialization
+    public void Init()
+    {
+        playerRecords = ClientPlayersManager.Players.ToDictionary(v => v.playerModel.playerId, _ => new PlayerRecord());
+
+        if (isServer)
+        {
+            var killLogNotifiers = ClientPlayersManager.Instance.GetPlayersComponent<PlayerKillLogNotifier>();
+            var allKillLogStream = Observable.Merge(killLogNotifiers.Select(v => v.GetKillLogStream()))
+                .Subscribe(v =>
+                    {
+                        RpcUpdatePlayerReport(v.Key, v.Value);
+                    }
+                );
+        }
+    }
+
+    [ClientRpc]
+    void RpcUpdatePlayerReport(int killerId,int victimId)
+    {
+        playerRecords[killerId].killCount++;
+        playerRecords[victimId].deathCount++;
+
+        ReportKillStats();//デバッグ用　リザルト画面のVが出来たら消す
+    }
+
+    void ReportKillStats()
+    {
+        foreach (var record in playerRecords)
+        {
+            Debug.Log(string.Format("Plaryer{0} K:{1} D:{2}", record.Key, record.Value.killCount, record.Value.deathCount));
+        }
+    }
+}
