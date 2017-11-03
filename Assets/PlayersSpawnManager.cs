b9581b274ff65429e724f27254fd8ff8eefd7e8d
NB29979 <syhkn915@ybb.ne.jp>
2017-11-03 23:51:47 +0900

    NetworkManagerを継承したPlayersSpawnManagerを設置

---
+++ Assets/PlayersSpawnManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayersSpawnManager : NetworkManager
+{
+    [SerializeField] private List<GameObject> points;
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        var playerSpawnPos = new Vector3(0, 0, 0);
+        var player = Instantiate(playerPrefab, playerSpawnPos, Quaternion.identity);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+}
