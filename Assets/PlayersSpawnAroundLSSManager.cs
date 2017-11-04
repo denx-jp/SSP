a39df7596b45031b0de610e2c6dd43af95b457d0
NB29979 <syhkn915@ybb.ne.jp>
2017-11-04 22:41:10 +0900

    スポーンポイントがインスペクタから調整される距離に依存するように

--- Assets/PlayersSpawnAroundLSSManager.cs
+++ Assets/PlayersSpawnAroundLSSManager.cs
@@ -10,10 +10,16 @@ public class PlayersSpawnAroundLSSManager : MonoBehaviour
 
     private Dictionary<Transform,List<Transform>> spawnPointsDic;
 
-    public void Init()
+    void Start()
     {
         spawnPointsDic = new Dictionary<Transform, List<Transform>>();
 
+        this.ObserveEveryValueChanged(_ => distance)
+            .Subscribe(v => SetSpawnPoints(v));
+    }
+
+    public void Init()
+    {
         foreach (var lss in LifeSupportSystemObjects)
             InitSpawnPoints(lss);
     }
@@ -27,5 +33,29 @@ public class PlayersSpawnAroundLSSManager : MonoBehaviour
             spawnPoints.Add(spawnPoint);
 
         spawnPointsDic.Add(lss, spawnPoints);
+
+        SetSpawnPoints(1.0f);
+    }
+
+    private void SetSpawnPoints(float distance)
+    {
+        foreach(var pointPair in spawnPointsDic)
+        {
+            foreach(var point in pointPair.Value)
+            {
+                Vector3 relative = pointPair.Key.position - point.position;
+                Vector3.Normalize(relative);
+                float angle = 90 - Vector3.Angle(relative, transform.forward);
+
+                Vector3 newPosition = 
+                    new Vector3(distance * Mathf.Sin(Mathf.Deg2Rad*angle),
+                                0,
+                                distance * Mathf.Cos(Mathf.Deg2Rad * angle));
+
+                point.transform.position = pointPair.Key.position + newPosition;
+
+                Debug.Log(pointPair.Key.position + " " + point.position + " " + angle);
+            }
+        }
     }
 }
