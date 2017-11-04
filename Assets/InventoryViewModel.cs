bd1d9b10beb877812c8a44605ae26ebc4ef771bd
chromee <odk.mmmm@gmail.com>
2017-11-04 23:14:52 +0900

    装備中の武器のパネルにアウトラインをつける

--- Assets/InventoryViewModel.cs
+++ Assets/InventoryViewModel.cs
@@ -1,38 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UniRx;
 
 public class InventoryViewModel : MonoBehaviour
 {
+    private struct WeaponPanel
+    {
+        public WeaponViewModel weaponVM;
+        public Outline outline;
+        public WeaponPanel(WeaponViewModel vm, Outline ol)
+        {
+            weaponVM = vm;
+            outline = ol;
+        }
+    }
+
     [HideInInspector] public PlayerInventory inventory;
-    [SerializeField] private List<WeaponViewModel> weaponVM = new List<WeaponViewModel>();
+    [SerializeField] private List<GameObject> weaponPanels = new List<GameObject>();
     [SerializeField] private Sprite defaultImage;
 
-    private Dictionary<InventoryType, WeaponViewModel> viewMap = new Dictionary<InventoryType, WeaponViewModel>();
+    private Dictionary<InventoryType, WeaponPanel> viewMap = new Dictionary<InventoryType, WeaponPanel>();
 
     void Start()
     {
-        foreach (var view in weaponVM)
+        foreach (var weaponPanel in weaponPanels)
         {
-            viewMap[view.type] = view;
+            var view = weaponPanel.GetComponent<WeaponViewModel>();
+            var outline = weaponPanel.GetComponentInChildren<Outline>();
             view.SetImage(defaultImage);
+            viewMap[view.type] = new WeaponPanel(view, outline);
         }
     }
 
     public void Init()
     {
         // Initされる前から所持している武器のViewを更新
-        inventory.weapons.ToObservable().Subscribe(v => UpdateView(v.Key, v.Value.model));
+        inventory.weapons.ToObservable().Subscribe(v => UpdateView(v.Key, v.Value.model.image));
 
-        inventory.weapons.ObserveReplace().Subscribe(v => UpdateView(v.Key, v.NewValue.model));
-        inventory.weapons.ObserveAdd().Subscribe(v => UpdateView(v.Key, v.Value.model));
-        //inventory.weapons.ObserveRemove().Subscribe(v => UpdateView(v.Key, defaultModel));
+        inventory.weapons.ObserveReplace().Subscribe(v => UpdateView(v.Key, v.NewValue.model.image));
+        inventory.weapons.ObserveAdd().Subscribe(v => UpdateView(v.Key, v.Value.model.image));
+        inventory.weapons.ObserveRemove().Subscribe(v => UpdateView(v.Key, defaultImage));
+
+        this.ObserveEveryValueChanged(_ => inventory.currentWeaponType)
+            .Subscribe(type =>
+            {
+                foreach (var weaponPanel in viewMap)
+                {
+                    if (weaponPanel.Key == type)
+                        weaponPanel.Value.outline.enabled = true;
+                    else
+                        weaponPanel.Value.outline.enabled = false;
+                }
+            });
     }
 
-    private void UpdateView(InventoryType type, WeaponModel model)
+    private void UpdateView(InventoryType type, Sprite image)
     {
         var view = viewMap[type];
-        view.SetImage(model.image);
+        view.weaponVM.SetImage(image);
     }
 }
