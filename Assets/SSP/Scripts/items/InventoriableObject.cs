99e6cc7a9212df1225cbd791afd049de347412ce
chromee <odk.mmmm@gmail.com>
2017-11-06 00:36:47 +0900

    デフォルト装備の初期設定修正

--- Assets/SSP/Scripts/items/InventoriableObject.cs
+++ Assets/SSP/Scripts/items/InventoriableObject.cs
@@ -19,7 +19,7 @@ public class InventoriableObject : NetworkBehaviour, IInteractable
     void Start()
     {
         networkIdentity = GetComponent<NetworkIdentity>();
-        DefaultWeaponSetup();
+        StartCoroutine(DefaultWeaponSetup());
     }
 
     [Server]
@@ -52,10 +52,12 @@ public class InventoriableObject : NetworkBehaviour, IInteractable
 
     //所持中の武器を持ち主のインベントリに格納・装備する
     [ClientCallback]
-    private void DefaultWeaponSetup()
+    IEnumerator DefaultWeaponSetup()
     {
+        yield return new WaitForSeconds(1);     // ownerPlayerId(SyncVar)の同期に少し時間がかかるらしいので待つ。
+
         var owner = ClientScene.FindLocalObject(ownerPlayerId);
-        if (owner == null) return;
+        if (owner == null) yield break;
         var pim = owner.GetComponent<PlayerInventoryManager>();
 
         pim.SetWeaponToInventory(this.gameObject, model.type);
