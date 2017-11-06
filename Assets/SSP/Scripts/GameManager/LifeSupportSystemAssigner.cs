7a34924f861b7a655cb59e091dd60f90e44d8287
NB29979 <syhkn915@ybb.ne.jp>
2017-11-07 01:27:44 +0900

    GMが各チームのLSSのチームidとそのTransformを知っているように

--- Assets/SSP/Scripts/GameManager/LifeSupportSystemAssigner.cs
+++ Assets/SSP/Scripts/GameManager/LifeSupportSystemAssigner.cs
@@ -8,32 +8,39 @@ using UniRx;
 
 public class LifeSupportSystemAssigner : NetworkBehaviour
 {
-    [SerializeField]private Transform team1LifeSupportSystemTransform;
-    [SerializeField]private Transform team2LifeSupportSystemTransform;
-
-    [SerializeField]private PlayersSpawnAroundLSSManager playersSpawnAroundLSSManager; 
-
+    [SerializeField] private List<Transform> LSSTransforms;
     private List<GameObject> LSSAssignPointObjectList;
+    public Dictionary<int, Transform> LSSPositionDic;
 
     [ServerCallback]
     void Start()
     {
+        LSSPositionDic = new Dictionary<int, Transform>();
         LSSAssignPointObjectList = GameObject.FindGameObjectsWithTag(TagMap.Respawn).ToList();
 
         if (LSSAssignPointObjectList.Count >= 2)
         {
-            SetPoint(team1LifeSupportSystemTransform);
-            SetPoint(team2LifeSupportSystemTransform);
+            foreach(var LSSTransform in LSSTransforms)
+                SetPoint(LSSTransform);
 
-            playersSpawnAroundLSSManager.Init();
+            UpdateLSSPositionDic();
         }
     }
 
-    void SetPoint(Transform LSSTransform)
+    void SetPoint(Transform _LSSTransform)
     {
         int candidatePoint = UnityEngine.Random.Range(0, LSSAssignPointObjectList.Count - 1);
 
-        LSSTransform.position = LSSAssignPointObjectList[candidatePoint].transform.position;
+        _LSSTransform.position = LSSAssignPointObjectList[candidatePoint].transform.position;
         LSSAssignPointObjectList.Remove(LSSAssignPointObjectList[candidatePoint]);
+    }
+
+    public void UpdateLSSPositionDic()
+    {
+        foreach(var LSSTransform in LSSTransforms)
+        {
+            var LSSTeamId = LSSTransform.GetComponent<LifeSupportSystemModel>().GetTeamId();
+            LSSPositionDic[LSSTeamId] = LSSTransform;
+        }
     }
 }
