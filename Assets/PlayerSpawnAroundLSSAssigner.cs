2af1ab53600f1d613e35abad355cab229d1c3e2e
NB29979 <syhkn915@ybb.ne.jp>
2017-11-07 01:44:24 +0900

    プレイヤーがLSSの周りにスポーンするメソッドをプレイヤーに持たせる

---
+++ Assets/PlayerSpawnAroundLSSAssigner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnAroundLSSAssigner : MonoBehaviour
+{
+    void SpawnAroundLSS()
+    {
+        var teamId = gameObject.GetComponent<PlayerModel>().teamId;
+    }
+}
