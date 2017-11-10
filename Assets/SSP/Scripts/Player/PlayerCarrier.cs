a94e39aab86b32a511512a08152113157f595f5a
chromee <odk.mmmm@gmail.com>
2017-11-11 01:47:29 +0900

    LSSがプレイヤーについてくるように

---
+++ Assets/SSP/Scripts/Player/PlayerCarrier.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UniRx;
+using System.Linq;
+using UnityEngine.Networking;
+using RootMotion.FinalIK;
+
+public class PlayerCarrier : MonoBehaviour
+{
+    [SerializeField] private float searchRadius;
+    [SerializeField] private Transform holdPoint;
+    private PlayerInputManager pim;
+    private InteractionSystem interactionSystem;
+
+    private bool isHolding { get { return interactionSystem.IsPaused(FullBodyBipedEffector.LeftHand); } }
+    private CarriableObject carriableObject;
+
+    void Start()
+    {
+        interactionSystem = GetComponent<InteractionSystem>();
+        pim = GetComponent<PlayerInputManager>();
+
+        pim.Action2ButtonDown
+            .Where(v => v)
+            .Subscribe(v =>
+            {
+                if (!isHolding)
+                    SearchCarriable();
+                else
+                    Drop();
+            });
+
+    }
+
+    void SearchCarriable()
+    {
+        var castResult = Physics.OverlapSphere(this.transform.position, searchRadius);
+
+        if (castResult.Length <= 0) return;
+
+        var carriableObjects = castResult
+            .Where(v => v.gameObject.GetComponent<CarriableObject>() != null)
+            .Where(v => v.gameObject.GetComponent<CarriableObject>().CanCarry)
+            .OrderBy(v => Vector3.Distance(v.transform.position, this.transform.position));
+
+        if (carriableObjects.Count() <= 0) return;
+
+        carriableObject = carriableObjects.First().gameObject.GetComponent<CarriableObject>();
+        carriableObject.Pickup(interactionSystem, holdPoint);
+    }
+
+    void Drop()
+    {
+        carriableObject.Drop();
+    }
+}
---
+++ Assets/SSP/Scripts/items/CarriableObject.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion;
+using RootMotion.FinalIK;
+
+public class CarriableObject : MonoBehaviour
+{
+    [SerializeField] private Transform pivot;
+    [SerializeField] private float pickUpTime = 0.3f;
+    [HideInInspector] public InteractionObject obj;
+
+    private InteractionSystem interactionSystem;
+    private Transform holdPoint;
+    private float holdWeight, holdWeightVel;
+    private Vector3 pickUpPosition;
+    private Quaternion pickUpRotation;
+
+    public bool CanCarry = true;
+
+    private bool isHolding { get { return interactionSystem != null && interactionSystem.IsPaused(FullBodyBipedEffector.LeftHand); } }
+
+    public void Pickup(InteractionSystem _interactionSystem, Transform _holdPoint)
+    {
+        CanCarry = false;
+        interactionSystem = _interactionSystem;
+        holdPoint = _holdPoint;
+
+        // Listen to interaction events
+        interactionSystem.OnInteractionStart += OnStart;
+        interactionSystem.OnInteractionPause += OnPause;
+        interactionSystem.OnInteractionResume += OnDrop;
+
+        // オブジェクトがキャラクターの動きを継承するようにする
+        obj.transform.parent = interactionSystem.transform;
+
+        interactionSystem.StartInteraction(FullBodyBipedEffector.LeftHand, obj, false);
+        interactionSystem.StartInteraction(FullBodyBipedEffector.RightHand, obj, false);
+    }
+
+    public void Drop()
+    {
+        interactionSystem.ResumeAll();
+
+        interactionSystem.OnInteractionStart -= OnStart;
+        interactionSystem.OnInteractionPause -= OnPause;
+        interactionSystem.OnInteractionResume -= OnDrop;
+
+        interactionSystem = null;
+        holdPoint = null;
+
+        CanCarry = true;
+    }
+
+    void Start()
+    {
+        CanCarry = true;
+        obj = GetComponent<InteractionObject>();
+    }
+
+    // インタラクションが一時停止されたとき（トリガーの場合）、InteractionSystemによって呼び出されます。
+    private void OnPause(FullBodyBipedEffector effectorType, InteractionObject interactionObject)
+    {
+        if (effectorType != FullBodyBipedEffector.LeftHand) return;
+        if (interactionObject != obj) return;
+
+
+        // Make the object kinematic
+        var r = obj.GetComponent<Rigidbody>();
+        if (r != null) r.isKinematic = true;
+
+        // Set object pick up position and rotation to current
+        pickUpPosition = obj.transform.position;
+        pickUpRotation = obj.transform.rotation;
+        holdWeight = 0f;
+        holdWeightVel = 0f;
+    }
+
+    // インタラクションの開始時にInteractionSystemによって呼び出されます。
+    private void OnStart(FullBodyBipedEffector effectorType, InteractionObject interactionObject)
+    {
+        if (effectorType != FullBodyBipedEffector.LeftHand) return;
+        if (interactionObject != obj) return;
+
+        // Rotate the pivot of the hand targets
+        RotatePivot();
+
+        // Rotate the hold point so it matches the current rotation of the object
+        holdPoint.rotation = obj.transform.rotation;
+    }
+
+    // 相互作用が再開されて一時停止されたときにInteractionSystemによって呼び出されます。
+    private void OnDrop(FullBodyBipedEffector effectorType, InteractionObject interactionObject)
+    {
+        if (effectorType != FullBodyBipedEffector.LeftHand) return;
+        if (interactionObject != obj) return;
+
+        // Make the object independent of the character
+        obj.transform.parent = null;
+
+        // Turn on physics for the object
+        if (obj.GetComponent<Rigidbody>() != null) obj.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    private void RotatePivot()
+    {
+        // キャラクターに向かって平らな方向を得る
+        Vector3 characterDirection = (pivot.position - interactionSystem.transform.position).normalized;
+        characterDirection.y = 0f;
+
+        // オブジェクトのローカル空間に方向を変換する
+        Vector3 characterDirectionLocal = obj.transform.InverseTransformDirection(characterDirection);
+
+        // QuaTools.GetAxisは任意の方向に対して90度の正射投影軸を返します
+        Vector3 axis = QuaTools.GetAxis(characterDirectionLocal);
+        Vector3 upAxis = QuaTools.GetAxis(obj.transform.InverseTransformDirection(interactionSystem.transform.up));
+
+        // Rotate towards axis and upAxis
+        pivot.localRotation = Quaternion.LookRotation(axis, upAxis);
+    }
+
+    void LateUpdate()
+    {
+        if (isHolding)
+        {
+            // Smoothing in the hold weight
+            holdWeight = Mathf.SmoothDamp(holdWeight, 1f, ref holdWeightVel, pickUpTime);
+
+            // Interpolation
+            obj.transform.position = Vector3.Lerp(pickUpPosition, holdPoint.position, holdWeight);
+            obj.transform.rotation = Quaternion.Lerp(pickUpRotation, holdPoint.rotation, holdWeight);
+        }
+    }
+}
