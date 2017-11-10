d5cc04a71d9b2fd956ac3339b72d2ea83fb4f9c4
chromee <odk.mmmm@gmail.com>
2017-11-10 13:36:29 +0900

    ゲーム開始時に武器が生成されるように

--- Assets/SSP/Scripts/GameManager/GameManager.cs
+++ Assets/SSP/Scripts/GameManager/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : NetworkBehaviour
     [SerializeField] private GameJudger gameJudger;
     [SerializeField] private KillLogManager killLogManager;
     [SerializeField] private EtherPopper etherPopper;
+    [SerializeField] private WeaponPopper weaponPopper;
 
     [SerializeField] private Text message;
     [SerializeField] private GameObject StartPanel;
@@ -61,8 +62,7 @@ public class GameManager : NetworkBehaviour
         yield return new WaitForSeconds(2);
 
         RpcPrepareGame();
-        //初期設定
-        //武器を生成 
+        weaponPopper.Init();
         //プレイヤーをLSS周辺に移動
 
         yield return new WaitForSeconds(startDelay);
@@ -81,6 +81,7 @@ public class GameManager : NetworkBehaviour
 
         //戦闘開始
         RpcBattleStart();
+        etherPopper.Init();
         yield return new WaitForSeconds(1);
         RpcChangeMessage(string.Empty);
     }
@@ -111,7 +112,6 @@ public class GameManager : NetworkBehaviour
     void RpcBattleStart()
     {
         isGameStarting = true;
-        etherPopper.Init();
         team1LSS.GetComponent<LifeSupportSystemEtherManager>().Init();
         team2LSS.GetComponent<LifeSupportSystemEtherManager>().Init();
         message.text = "Battle Start";
---
+++ Assets/SSP/Scripts/GameManager/WeaponPopper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+using UniRx;
+
+public class WeaponPopper : NetworkBehaviour
+{
+    [SerializeField] private int weaponCount = 36;
+    [SerializeField] private List<GameObject> popWeapons;
+    private Stack<Transform> popPoints = new Stack<Transform>();
+
+    public void Init()
+    {
+        if (isServer)
+        {
+            GameObject.FindGameObjectsWithTag(TagMap.PopPoint).Select(v => v.transform)
+                .OrderBy(i => System.Guid.NewGuid()).ToList().ForEach(v => popPoints.Push(v));
+            var spawnCoiunt = weaponCount < popPoints.Count ? weaponCount : popPoints.Count;
+            for (int i = 0; i < spawnCoiunt; i++)
+            {
+                var popPoint = popPoints.Pop();
+                var popWeapon = popWeapons[Random.Range(0, popWeapons.Count - 1)];
+                var weapon = Instantiate(popWeapon, popPoint.position + Vector3.up, popPoint.rotation);
+                NetworkServer.Spawn(weapon);
+            }
+        }
+    }
+}
