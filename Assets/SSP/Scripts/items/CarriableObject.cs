5692d7cff1a07cf0cc771c83c4c85290ab146096
chromee <odk.mmmm@gmail.com>
2017-11-15 18:22:04 +0900

    コメント削除

--- Assets/SSP/Scripts/items/CarriableObject.cs
+++ Assets/SSP/Scripts/items/CarriableObject.cs
@@ -39,10 +39,8 @@ public class CarriableObject : MonoBehaviour
             .Where(_ => holdWeight < 0.999)     //< 0.999を1とみなす
             .Subscribe(_ =>
             {
-                // Smoothing in the hold weight
                 holdWeight = Mathf.SmoothDamp(holdWeight, 1f, ref holdWeightVel, pickUpTime);
-
-                // Interpolation
+                
                 transform.position = Vector3.Lerp(pickUpPosition, holdPoint.position, holdWeight);
                 transform.rotation = Quaternion.Lerp(pickUpRotation, holdPoint.rotation, holdWeight);
             });
@@ -60,13 +58,11 @@ public class CarriableObject : MonoBehaviour
 
         interactionSystem = _interactionSystem;
         holdPoint = _holdPoint;
-
-        // Listen to interaction events
+        
         interactionSystem.OnInteractionStart += OnStart;
         interactionSystem.OnInteractionPause += OnPause;
         interactionSystem.OnInteractionResume += OnDrop;
-
-        // オブジェクトがキャラクターの動きを継承するようにする
+        
         networkTransform.enabled = false;
         transform.parent = interactionSystem.transform;
 
@@ -88,50 +84,40 @@ public class CarriableObject : MonoBehaviour
         gameObject.layer = defaultLayer;
         canCarry = true;
     }
-
-    // インタラクションが一時停止されたとき（トリガーの場合）、InteractionSystemによって呼び出されます。
+    
     private void OnPause(FullBodyBipedEffector effectorType, InteractionObject interactionObject)
     {
         if (effectorType != FullBodyBipedEffector.LeftHand) return;
         if (interactionObject != obj) return;
-
-        // Make the object kinematic
+        
         rigid.isKinematic = true;
-
-        // Set object pick up position and rotation to current
+        
         pickUpPosition = transform.position;
         pickUpRotation = transform.rotation;
         holdWeight = 0f;
         holdWeightVel = 0f;
     }
-
-    // インタラクションの開始時にInteractionSystemによって呼び出されます。
+    
     private void OnStart(FullBodyBipedEffector effectorType, InteractionObject interactionObject)
     {
         if (effectorType != FullBodyBipedEffector.LeftHand) return;
         if (interactionObject != obj) return;
 
         #region Pivotの回転
-        // キャラクターに向かって平らな方向を得る
         Vector3 characterDirection = (pivot.position - interactionSystem.transform.position).normalized;
         characterDirection.y = 0f;
-
-        // オブジェクトのローカル空間に方向を変換する
+        
         Vector3 characterDirectionLocal = obj.transform.InverseTransformDirection(characterDirection);
-
-        // QuaTools.GetAxisは任意の方向に対して90度の正射投影軸を返します
+        
         Vector3 axis = QuaTools.GetAxis(characterDirectionLocal);
         Vector3 upAxis = QuaTools.GetAxis(obj.transform.InverseTransformDirection(interactionSystem.transform.up));
-
-        // Rotate towards axis and upAxis
+        
         pivot.localRotation = Quaternion.LookRotation(axis, upAxis);
         #endregion
-
-        // Rotate the hold point so it matches the current rotation of the object
+        
         holdPoint.rotation = transform.rotation;
     }
-
-    // 相互作用が再開されて一時停止されたときにInteractionSystemによって呼び出されます。
+    
     private void OnDrop(FullBodyBipedEffector effectorType, InteractionObject interactionObject)
     {
         if (effectorType != FullBodyBipedEffector.LeftHand) return;
