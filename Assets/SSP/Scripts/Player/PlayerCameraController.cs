ebfaa89cb7dec81fee07035159f21c2cad1af68e
chromee <odk.mmmm@gmail.com>
2017-11-24 04:39:36 +0900

    プレイヤーとカメラの間に障害物がきたときにカメラを近づける

--- Assets/SSP/Scripts/Player/PlayerCameraController.cs
+++ Assets/SSP/Scripts/Player/PlayerCameraController.cs
@@ -10,42 +10,70 @@ public class PlayerCameraController : MonoBehaviour
 {
     [SerializeField] private PlayerHealthManager healthManager;
     [SerializeField] private PlayerInputManager pim;
-    [SerializeField] private Transform target;
     [SerializeField] private PlayerInventory inventory;
+    [SerializeField] private Transform target;
+    [SerializeField] private float correctPosSpeed = 2f;
+    [SerializeField] private float yMinLimit = -80;
+    [SerializeField] private float yMaxLimit = 80;
+    [SerializeField] private float rotationSensitivity = 2.0f;
+    [SerializeField] private float battleRotationSensitivity = 2.0f;
+    [SerializeField] private float scopeRotationSensitivity = 2.0f;
+    [SerializeField] private float battleMagnification = 1.0f;
+
+    [SerializeField] private Vector3 normalModeOffset = new Vector3(0, 0, -3);
+    [SerializeField] private Vector3 balltleModeOffset = new Vector3(0.5f, 1.5f, -1.5f);
+    [SerializeField] private Vector3 scopeModeOffset = new Vector3(0, 0.1f, 0.3f);
+
     private CameraMode mode;
+    private Vector3 offset;
     private float defaultFieldOfView;
     private Camera mainCamera;
-
-    [Header("Noraml Mode")]
-    [SerializeField]
-    private float cameraRotationSpeed = 100;
-    [SerializeField] private Vector3 normalModeOffset = new Vector3(0, 0, -3);
     private Transform cameraTransform;
-    private Vector3 tempOffset;
-
-    [Header("Battle Mode")]
-    [SerializeField]
-    private float yMinLimit = -80; // Min vertical angle
-    [SerializeField] private float yMaxLimit = 80; // Max vertical angle
-    [SerializeField] private float rotationSensitivity = 3.5f; // The sensitivity of rotation
-    [SerializeField] private Vector3 balltleModeOffset = new Vector3(0.5f, 1.5f, -1.5f); // The offset from target relative to camera rotation
-    [SerializeField] private float battleMagnification = 1.0f;
     private float x, y;
-
-    [Header("Scope Mode")]
-    [SerializeField]
-    private float scopeRotationSensitivity = 3.5f;
-    [HideInInspector] private Vector3 scopeModeOffset = new Vector3(0, 0.1f, 0.3f);
 
     private void Start()
     {
         var model = GetComponent<PlayerModel>();
         if (!model.isLocalPlayerCharacter) return;
 
+        mode = CameraMode.Normal;
         mainCamera = Camera.main;
         cameraTransform = mainCamera.transform;
-        mode = CameraMode.Normal;
         defaultFieldOfView = mainCamera.fieldOfView;
+
+        this.ObserveEveryValueChanged(_ => mode)
+            .Subscribe(_ =>
+            {
+                switch (mode)
+                {
+                    case CameraMode.Normal:
+                        offset = normalModeOffset;
+                        break;
+                    case CameraMode.Battle:
+                        offset = balltleModeOffset;
+                        break;
+                    case CameraMode.Scope:
+                        offset = scopeModeOffset;
+                        break;
+                }
+            });
+
+        // targetとカメラの間に障害物があった時にカメラをtargetに近づける
+        this.UpdateAsObservable()
+            .Subscribe(_ =>
+            {
+                RaycastHit hit;
+                var dir = cameraTransform.position - target.position;
+                if (Physics.Raycast(target.position, dir, out hit, normalModeOffset.magnitude, LayerMap.StageMask))
+                {
+                    var distance = Vector3.Distance(hit.point, target.position) * 0.8f;
+                    offset = Vector3.Lerp(offset, offset.normalized * distance, Time.deltaTime * correctPosSpeed);
+                }
+                else
+                {
+                    offset = Vector3.Lerp(offset, normalModeOffset, Time.deltaTime * correctPosSpeed);
+                }
+            });
 
         healthManager.GetDeathStream()
             .Where(v => v)
@@ -56,8 +84,14 @@ public class PlayerCameraController : MonoBehaviour
                 LookPlayer();
             });
 
-        #region Normal Mode
-        tempOffset = normalModeOffset;
+        pim.CameraRotate
+            .Where(v => target != null)
+            .Subscribe(input =>
+            {
+                x += input.x * rotationSensitivity;
+                y = ClampAngle(y - input.y * rotationSensitivity, yMinLimit, yMaxLimit);
+                LookPlayer();
+            });
 
         pim.CameraResetButtonDown
             .Where(_ => mode == CameraMode.Normal)
@@ -65,62 +99,18 @@ public class PlayerCameraController : MonoBehaviour
             .Where(_ => target != null)
             .Subscribe(_ =>
             {
-                var targetDir = target.transform.forward;
-                targetDir = new Vector3(targetDir.x, 0.0f, targetDir.z);
-                var rotation = Quaternion.LookRotation(targetDir, Vector3.up);
-                tempOffset = rotation * normalModeOffset;
+                x = transform.eulerAngles.y;
+                y = transform.eulerAngles.x;
             });
-
-        pim.CameraRotate
-            .Where(_ => mode == CameraMode.Normal)
-            .Where(v => target != null)
-            .Subscribe(input =>
-            {
-                tempOffset = Quaternion.Euler(0.0f, input.x * Time.deltaTime * cameraRotationSpeed, 0.0f) * tempOffset;
-
-                //ジンバルロックしないように制御
-                var temp_delta = target.transform.position - cameraTransform.position;
-                if ((Vector3.Dot(temp_delta, new Vector3(temp_delta.x, 0.0f, temp_delta.z))) > 0.1f)
-                {
-                    tempOffset = Quaternion.AngleAxis(-1.0f * input.y * Time.deltaTime * cameraRotationSpeed, cameraTransform.right) * tempOffset;
-                }
-                else
-                {
-                    if (temp_delta.y > 0.0f && input.y < 0.0f)
-                        tempOffset = Quaternion.AngleAxis(-1.0f * input.y * Time.deltaTime * cameraRotationSpeed, cameraTransform.right) * tempOffset;
-                    else if (temp_delta.y < 0.0f && input.y > 0.0f)
-                        tempOffset = Quaternion.AngleAxis(-1.0f * input.y * Time.deltaTime * cameraRotationSpeed, cameraTransform.right) * tempOffset;
-                }
-
-                LookPlayer();
-            });
-        #endregion
-
-        #region Battle/Scope Mode
-        pim.CameraRotate
-            .Where(_ => mode != CameraMode.Normal)
-            .Where(_ => target != null)
-            .Subscribe(input =>
-            {
-                x += input.x * rotationSensitivity;
-                y = ClampAngle(y - input.y * rotationSensitivity, yMinLimit, yMaxLimit);
-                var offset = mode == CameraMode.Battle ? balltleModeOffset : scopeModeOffset;
-
-                var rotation = Quaternion.AngleAxis(x, Vector3.up) * Quaternion.AngleAxis(y, Vector3.right);
-                var position = target.position + rotation * offset;
-
-                cameraTransform.position = position;
-                cameraTransform.rotation = rotation;
-            });
-        #endregion
     }
 
     public void LookPlayer()
     {
-        cameraTransform.position = target.transform.position + tempOffset;
-        var delta = target.transform.position - cameraTransform.position;
-        var direction = new Vector3(delta.x, delta.y + normalModeOffset.y, delta.z);
-        cameraTransform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        var rotation = Quaternion.AngleAxis(x, Vector3.up) * Quaternion.AngleAxis(y, Vector3.right);
+        var position = target.position + rotation * offset;
+
+        cameraTransform.position = position;
+        cameraTransform.rotation = rotation;
     }
 
     private float ClampAngle(float angle, float min, float max)
