d0f6b5685af2a0f409aed27fc5dfcf65cc282993
chromee <odk.mmmm@gmail.com>
2017-11-24 00:19:14 +0900

    ステージを透過させるスクリプト作成

---
+++ Assets/SSP/Scripts/Function/StageTransparenter.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class StageTransparenter : MonoBehaviour
+{
+    [SerializeField] private float transparentValue = 0.5f;
+
+    void Start()
+    {
+        var alpha = new Color(0, 0, 0, transparentValue);
+
+        this.OnTriggerEnterAsObservable()
+            .Where(col => col.gameObject.layer == LayerMap.Stage)
+            .Subscribe(col =>
+            {
+                var renderer = col.GetComponent<Renderer>();
+                if (renderer != null)
+                    renderer.materials.ToList().ForEach(v => v.color -= alpha);
+            });
+
+        this.OnTriggerExitAsObservable()
+            .Where(col => col.gameObject.layer == LayerMap.Stage)
+            .Subscribe(col =>
+            {
+                var renderer = col.GetComponent<Renderer>();
+                if (renderer != null)
+                    renderer.materials.ToList().ForEach(v => v.color += alpha);
+            });
+    }
+
+}
