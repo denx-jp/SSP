1ccdecd7d968ef7f4bc4a1e9843d031628f67a71
chromee <odk.mmmm@gmail.com>
2017-11-24 12:20:31 +0900

    ガイド表示（途中）

--- Assets/SSP/Scripts/Player/PlayerCameraController.cs
+++ Assets/SSP/Scripts/Player/PlayerCameraController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 using System.Linq;
---
+++ Assets/SSP/Scripts/items/GuideDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class GuideDetector : MonoBehaviour
+{
+    public Subject<GuideObject> guideDetectStream = new Subject<GuideObject>();
+
+    void Start()
+    {
+        this.OnTriggerStayAsObservable()
+            .Select(col => col.GetComponent<GuideObject>())
+            .Where(gObj => gObj != null)
+            .Subscribe(guideObject =>
+            {
+                guideDetectStream.OnNext(guideObject);
+            });
+    }
+}
